c9aca148585d2e7adf0dbf2f64cf2d99f6c99177
Ivan Sabelnikov <ivan.sabelnikov@gmail.com>
2021-03-10 14:35:11 +0200

    Add version increment

--- dist/index.js
+++ dist/index.js
@@ -356,33 +356,115 @@ const github = __importStar(__webpack_require__(438));
 const core = __importStar(__webpack_require__(186));
 function CreateReleaseTag(context, token) {
     return __awaiter(this, void 0, void 0, function* () {
-        if (token) {
-            const octokit = github.getOctokit(token);
-            const mainBranch = "main";
-            // TODO: This will only return first 30 results. Use pagination to search among releases.
-            const tags = yield octokit.request('GET /repos/{owner}/{repo}/tags', {
-                owner: context.repo.owner,
-                repo: context.repo.repo,
-            });
-            core.info(`Tags: ${JSON.stringify(tags)}`);
-            // await octokit.request('POST /repos/{owner}/{repo}/releases', {
-            //     owner: context.repo.owner,
-            //     repo: context.repo.repo,
-            //     tag_name: tag
-            // })
-            const commits = yield octokit.request('GET /repos/{owner}/{repo}/commits', {
-                owner: context.repo.owner,
-                repo: context.repo.repo,
-                sha: mainBranch
-            });
-            core.info(`Commits: ${JSON.stringify(commits)}`);
-            core.info(`Context: ${JSON.stringify(context)}`);
-            const tag = 'v1.0.0';
-            core.info(`Created tag '${tag}'`);
+        // TODO: Move into vars
+        const mainBranch = "main";
+        const initialTag = "v1.0.0";
+        const branchRegExp = new RegExp(`refs/heads/${mainBranch}`);
+        // Tagging is allowed only for main branch
+        if (!branchRegExp.test(context.ref)) {
+            return;
         }
+        if (!token) {
+            return;
+        }
+        const octokit = github.getOctokit(token);
+        // TODO: This will only return first 30 results. Use pagination to search among all releases.
+        const tags = yield octokit.request('GET /repos/{owner}/{repo}/tags', {
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+        });
+        // TODO: Remove
+        core.info(`Tags: ${JSON.stringify(tags)}`);
+        let latestVersion = null;
+        let latestVersionCommit = null;
+        tags.data.forEach(tag => {
+            const ver = Version.parse(tag.name);
+            if (ver == null) {
+                return;
+            }
+            if (latestVersion == null || ver.isGreaterThan(latestVersion)) {
+                latestVersion = ver;
+                latestVersionCommit = tag.commit.sha;
+            }
+        });
+        // TODO: Remove 
+        if (latestVersion != null) {
+            core.info(`latestVersion: ${latestVersion.toString()}, latestVersionCommit: ${latestVersionCommit.toString()}`);
+        }
+        if (latestVersion == null && initialTag) {
+            core.info(`Could not find any valid release tag. Trying to use initial tag from config...`);
+            latestVersion = Version.parse(initialTag);
+        }
+        if (latestVersion == null) {
+            core.info(`Could not find any valid release tag. Initial tag parameter is not set or invalid. Setting version to v1.0.0`);
+            latestVersion = Version.parse("v1.0.0");
+        }
+        const nextVersion = Version.parse(latestVersion.toString());
+        // TODO: Increment according to semantic commits
+        nextVersion === null || nextVersion === void 0 ? void 0 : nextVersion.incrementPatch();
+        var nextTagName = nextVersion.toString();
+        yield octokit.request('POST /repos/{owner}/{repo}/releases', {
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            tag_name: nextTagName
+        });
+        core.info(`Created a release with tag '${nextTagName}'`);
+        const commits = yield octokit.request('GET /repos/{owner}/{repo}/commits', {
+            owner: context.repo.owner,
+            repo: context.repo.repo,
+            sha: mainBranch
+        });
+        // TODO: Remove
+        core.info(`Commits: ${JSON.stringify(commits)}`);
+        // TODO: Remove
+        core.info(`Context: ${JSON.stringify(context)}`);
     });
 }
 exports.CreateReleaseTag = CreateReleaseTag;
+class Version {
+    constructor(major, minor, patch) {
+        this.major = major;
+        this.minor = minor;
+        this.patch = patch;
+    }
+    toString() {
+        return `v${this.major}.${this.minor}.${this.patch}`;
+    }
+    // TODO: Cover with tests
+    isGreaterThan(ver) {
+        if (this.major != ver.major) {
+            return this.major > ver.major;
+        }
+        if (this.minor != ver.minor) {
+            return this.minor > ver.minor;
+        }
+        if (this.patch != ver.patch) {
+            return this.patch > ver.patch;
+        }
+        return false;
+    }
+    // TODO: Cover with tests
+    incrementMajor() {
+        this.major++;
+    }
+    // TODO: Cover with tests
+    incrementMinor() {
+        this.minor++;
+    }
+    // TODO: Cover with tests
+    incrementPatch() {
+        this.patch++;
+    }
+    // TODO: Cover with tests
+    static parse(val) {
+        const res = Version.regexp.exec(val);
+        if (res == null) {
+            return null;
+        }
+        return new Version(parseInt(res.groups[0]), parseInt(res.groups[1]), parseInt(res.groups[2]));
+    }
+}
+Version.regexp = /v(\d+).(\d+).(\d+)/;
 
 
 /***/ }),
--- src/releasetag.ts
+++ src/releasetag.ts
@@ -4,36 +4,142 @@ import * as core from '@actions/core';
 
 export async function CreateReleaseTag(
     context: Context,
-    token: string | null
+    token: string | null,
 ) {
-    if (token) {
-        const octokit = github.getOctokit(token);
-        const mainBranch = "main"
+    // TODO: Move into vars
+    const mainBranch = "main"
+    const initialTag: string | null = "v1.0.0"
 
-        // TODO: This will only return first 30 results. Use pagination to search among releases.
-        const tags = await octokit.request('GET /repos/{owner}/{repo}/tags', {
-            owner: context.repo.owner,
-            repo: context.repo.repo,
-        });
+    const branchRegExp = new RegExp(`refs/heads/${mainBranch}`)
 
-        core.info(`Tags: ${JSON.stringify(tags)}`);
+    // Tagging is allowed only for main branch
+    if (!branchRegExp.test(context.ref)) {
+        return;
+    }
+    
+    if (!token) {
+        return;
+    }
+    
+    const octokit = github.getOctokit(token);
 
-        // await octokit.request('POST /repos/{owner}/{repo}/releases', {
-        //     owner: context.repo.owner,
-        //     repo: context.repo.repo,
-        //     tag_name: tag
-        // })
+    // TODO: This will only return first 30 results. Use pagination to search among all releases.
+    const tags = await octokit.request('GET /repos/{owner}/{repo}/tags', {
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+    });
 
-        const commits = await octokit.request('GET /repos/{owner}/{repo}/commits', {
-            owner: context.repo.owner,
-            repo: context.repo.repo,
-            sha: mainBranch
-        });
+    // TODO: Remove
+    core.info(`Tags: ${JSON.stringify(tags)}`);
 
-        core.info(`Commits: ${JSON.stringify(commits)}`);
+    let latestVersion: Version | null = null;
+    let latestVersionCommit: string | null = null;
 
-        core.info(`Context: ${JSON.stringify(context)}`);
-        const tag = 'v1.0.0';
-        core.info(`Created tag '${tag}'`);
+    tags.data.forEach(tag => {
+        const ver = Version.parse(tag.name);
+        if (ver == null) {
+            return;
+        }
+
+        if (latestVersion == null || ver.isGreaterThan(latestVersion)) {
+            latestVersion = ver;
+            latestVersionCommit = tag.commit.sha;
+        }
+    });
+
+    // TODO: Remove 
+    if (latestVersion != null) {
+        core.info(`latestVersion: ${latestVersion!.toString()}, latestVersionCommit: ${latestVersionCommit!.toString()}`);
+    }
+
+    if (latestVersion == null && initialTag) {
+        core.info(`Could not find any valid release tag. Trying to use initial tag from config...`);
+        latestVersion = Version.parse(initialTag);
+    }
+
+    if (latestVersion == null) {
+        core.info(`Could not find any valid release tag. Initial tag parameter is not set or invalid. Setting version to v1.0.0`);
+        latestVersion = Version.parse("v1.0.0");
+    }
+
+    const nextVersion = Version.parse(latestVersion!.toString());
+
+    // TODO: Increment according to semantic commits
+    nextVersion?.incrementPatch();
+    
+    var nextTagName = nextVersion!.toString();
+
+    await octokit.request('POST /repos/{owner}/{repo}/releases', {
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        tag_name: nextTagName
+    })
+
+    core.info(`Created a release with tag '${nextTagName}'`);
+
+    const commits = await octokit.request('GET /repos/{owner}/{repo}/commits', {
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        sha: mainBranch
+    });
+
+    // TODO: Remove
+    core.info(`Commits: ${JSON.stringify(commits)}`);
+
+    // TODO: Remove
+    core.info(`Context: ${JSON.stringify(context)}`);
+}
+
+class Version {
+    private static regexp = /v(\d+).(\d+).(\d+)/;
+
+    constructor(private major: number, private minor: number, private patch: number) {
+    }
+
+    toString(): string {
+        return `v${this.major}.${this.minor}.${this.patch}`;
+    }
+
+    // TODO: Cover with tests
+    isGreaterThan(ver: Version): boolean {
+        if (this.major != ver.major) {
+            return this.major > ver.major;
+        }
+
+        if (this.minor != ver.minor) {
+            return this.minor > ver.minor;
+        }
+
+        if (this.patch != ver.patch) {
+            return this.patch > ver.patch;
+        }
+
+        return false;
+    }
+
+    // TODO: Cover with tests
+    incrementMajor() {
+        this.major++;
+    }
+
+    // TODO: Cover with tests
+    incrementMinor() {
+        this.minor++;
+    }
+
+    // TODO: Cover with tests
+    incrementPatch() {
+        this.patch++;
+    }
+
+    // TODO: Cover with tests
+    static parse(val: string): Version | null {
+        const res = Version.regexp.exec(val);
+        if (res == null) {
+            return null;
+        }
+
+        return new Version(parseInt(res.groups![0]), parseInt(res.groups![1]), parseInt(res.groups![2]))
     }
 }
+
